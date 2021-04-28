5a22cf2ec5701477427a00efe1fd06eff8bf2a1c
KAWAHARA Keisuke <kyohsuke@users.noreply.github.com>
2021-04-28 19:23:31 +0900

    Tweaks

--- update.sh
+++ update.sh
@@ -3,11 +3,17 @@ set -euxo pipefail
 
 DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
 
-git submodule init
-git submodule update --remote --init --checkout
-
-mkdir -p "$DIR/syntax/"
 cd "$DIR"
-rm -rf "$DIR"/syntax/*
+rm -rf "$DIR/vim-go"
+rm -rf "$DIR/syntax"
+rm -rf "$DIR/ftdetect"
+
+git clone https://github.com/fatih/vim-go vim-go
+
+mkdir "$DIR/syntax/"
+mkdir "$DIR/ftdetect/"
+
+cp "$DIR"/vim-go/ftdetect/* "$DIR/ftdetect/"
 cp "$DIR"/vim-go/syntax/* "$DIR/syntax/"
 rm -f "$DIR/syntax/go.vim"
+rm -rf "$DIR/vim-go"
